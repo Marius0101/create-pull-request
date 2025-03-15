6da21e550fca86d5e1853434f5a6606d0e4e0055
Marius0101 <Marius_Mihalcea@waters.com>
2025-03-15 15:17:48 +0200

    clean code in main

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,19 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import { getInputs, createPullRequest } from './common';
 
 export async function run(): Promise<void> {
     
-    const ghToken:string = core.getInput("gh-token");
-    const title:string = core.getInput("title");
-    const head:string = core.getInput("head");
-    const base:string = core.getInput("base");
-    const body:string = core.getInput("body");
-    const octokit = github.getOctokit(ghToken);
+    const inputs = await getInputs();
+    const octokit = github.getOctokit(inputs.ghToken);
 
+    var pull_request_number:number;
     core.info("Creating the pull request")
-    try{
-    const pull_request = await octokit.rest.pulls.create({
-        owner: 'Marius0101',
-        repo: 'create-pull-request',
-        head: head,
-        base: base,
-        title: title,
-        body: body
-    })
-    core.info(`Pull request created successfully: ${pull_request.data.html_url}`);
+    const pr_number:number = await createPullRequest(inputs, octokit)
+    if(inputs){
+        
     }
-    catch(error){
-        if (error instanceof Error) {
-            core.setFailed(`\nAction failed: ${error.message}`);
-        } 
-        else {
-            core.setFailed('Action failed: Unknown error');
-        }
+    else{
+        core.info("No user was assinge.")
     }
-    
 }
