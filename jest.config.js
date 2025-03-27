9cfafefe404d15549acc00078267d8689160e0fe
Marius M <mariusoctavian010@gmail.com>
2025-03-27 21:27:17 +0200

    add junit test result report

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,8 @@ module.exports = {
     "^.+\.tsx?$": ["ts-jest",{}],
   },
   testMatch: ["**/?(*.)+(spec|test).ts?(x)"],
+  reporters: [
+    "default",
+    ["jest-junit", { outputDirectory: "./test-results", outputName: "jest-results.xml" }]
+  ]
 };
